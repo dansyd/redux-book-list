a6044e0138cab27ce73b136e050f3fbedf93c793
Daniele Peviani <daniele.peviani@gmail.com>
2017-03-25 16:31:13 +1100

    Created first reducer and container component

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Learning JavaScript Design Patterns' },
+    { title: 'Essentialism: The Disciplined Pursuit of Less' },
+    { title: 'The Power of Habit' },
+  ];
+}
